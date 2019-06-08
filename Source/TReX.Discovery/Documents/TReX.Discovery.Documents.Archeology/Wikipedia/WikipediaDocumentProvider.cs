1d9bccc803513d9f42fc67ea26cf42b1b2c5d0b3
MurariuF <murariu.flavius@gmail.com>
2019-06-08 16:30:50 +0300

    Half Provider

--- Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Wikipedia/WikipediaDocumentProvider.cs
+++ Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Wikipedia/WikipediaDocumentProvider.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using CSharpFunctionalExtensions;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace TReX.Discovery.Documents.Archeology.Wikipedia
 {
     class WikipediaDocumentProvider
     {
+        private readonly WikipediaSettings settings;
+
+        public async Task<Result<HttpResponseMessage>> Search(string query, string page)
+        {
+            var request = "https://en.wikipedia.org//w/api.php?action=query&format=json&list=search&srsearch=" +
+                          settings.SrSearch + "&srlimit=" + settings.SrLimit + "&sroffset=" + settings.SrOffSet + "&srwhat=" +
+                          settings.SrWhat;
+            return await Result.Try(() => request.ExecuteAsync());
+        }
     }
 }
