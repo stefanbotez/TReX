2851d0eb2859cfa358210a4082a5c95dc931ea14
MurariuF <murariu.flavius@gmail.com>
2019-06-08 16:51:58 +0300

    Update WikipediaDocumentLecture

--- Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Wikipedia/WikipediaDocumentLecture.cs
+++ Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Wikipedia/WikipediaDocumentLecture.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using TReX.Kernel.Shared.Domain;
+using TReX.Discovery.Documents.Domain;
 
 namespace TReX.Discovery.Documents.Archeology.Wikipedia
 {
-    class WikipediaDocumentLecturer
+    public sealed class WikipediaDocumentLecture : AggregateRoot, IDocumentLecure
     {
+        private WikipediaDocumentLecture()
+
+        public WikipediaDocumentLecture(int ns, string title, int pageid, int size, int wordcount, string snippet)
+        {
+            NS = ns;
+            Title = title;
+            PageId = pageid;
+            Size = size;
+            WordCount = wordcount;
+            Snippet = snippet;
+        }
+
+        public int NS { get; private set; }
+        public string Title { get; private set; }
+        public int PageId { get; private set; }
+        public int Size { get; private set; }
+        public int WordCount { get; private set; }
+        public string Snippet { get; private set; }
     }
+
+
+
 }
