9cceed455fb0f75509e228e7f55e100660263b22
Stefan Botez <stfn_botez@yahoo.com>
2019-06-08 10:48:15 +0300

    Github partially done

---
+++ Source/TReX.Discovery/Code/TReX.Discovery.Code.Archeology/Github/GithubCodeLecture.cs
@@ -0,0 +1,31 @@
+﻿using Octokit;
+using Octokit.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TReX.Discovery.Code.Archeology.Github
+{
+    class GithubCodeLecture
+    {
+        private GithubCodeLecture()
+        {
+        }
+
+        public GithubCodeLecture(Repository result) : this()
+        {
+            CodeId = result.Id.ToString();
+            Title = result.Name;
+            Description = result.Description;
+            PublishedAt = result.PushedAt.Value.DateTime;
+        }
+
+        public string CodeId { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Description { get; private set; }
+
+        public DateTime PublishedAt { get; private set; }
+    }
+}
