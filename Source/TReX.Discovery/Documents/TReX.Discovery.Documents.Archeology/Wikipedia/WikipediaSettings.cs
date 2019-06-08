28ed53ff1b7390c14dd42e4b451c22597ffef586
MurariuF <murariu.flavius@gmail.com>
2019-06-08 15:35:21 +0300

    Created classes for Wikipedia
    Settings done

---
+++ Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Wikipedia/WikipediaSettings.cs
@@ -0,0 +1,26 @@
+﻿using EnsureThat;
+
+namespace TReX.Discovery.Documents.Archeology.Wikipedia
+{
+    public sealed class WikipediaSettings
+    {
+        public WikipediaSettings(string srWhat, string srSearch,int srLimit, int srOffSet)
+        {
+            EnsureArg.IsNotNullOrWhiteSpace(srWhat);
+            EnsureArg.IsNotNullOrWhiteSpace(SrSearch);
+            EnsureArg.IsLte(srLimit, 10);
+            EnsureArg.IsLte(srOffSet, 2);
+
+            SrWhat = srWhat;
+            SrSearch = srSearch;
+            SrLimit = srLimit;
+            SrOffSet = srOffSet;
+
+        }
+
+        public string SrWhat { get; }
+        public string SrSearch { get; }
+        public int SrLimit { get; }
+        public int SrOffSet { get; }
+    }
+}
