10ac11c5225b78d4183a6d5bcfa6db3fb42be12f
Stefan Botez <stfn_botez@yahoo.com>
2019-06-10 08:43:54 +0300

    Added Twitter Archeologist

--- Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Constants.cs
+++ Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Constants.cs
@@ -3,5 +3,6 @@
     public static class Constants
     {
         public const string Wikipedia = nameof(Wikipedia);
+        public const string Twitter = nameof(Twitter);
     }
 }
---
+++ Source/TReX.Discovery/Documents/TReX.Discovery.Documents.Archeology/Twitter/TwitterSettings.cs
@@ -0,0 +1,40 @@
+﻿using EnsureThat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tweetinvi;
+
+namespace TReX.Discovery.Documents.Archeology.Twitter
+{
+    public class TwitterSettings
+    {
+        public TwitterSettings(string consumerKey, string consumerSecret, string apiKey, string apiSecret, int perPage, int maxDepth)
+        {
+            EnsureArg.IsNotNullOrWhiteSpace(consumerKey);
+            EnsureArg.IsNotNullOrWhiteSpace(consumerSecret);
+            EnsureArg.IsNotNullOrWhiteSpace(apiKey);
+            EnsureArg.IsNotNullOrWhiteSpace(apiSecret);
+
+            EnsureArg.IsGte(maxDepth, 1);
+
+            ConsumerKey = consumerKey;
+            ConsumerSecret = consumerSecret;
+            ApiKey = apiKey;
+            ApiSecret = apiSecret;
+            PerPage = perPage;
+            MaxDepth = maxDepth;
+        }
+
+        public string ConsumerKey { get; } //AlffsFObuC02EcAdmiNexiJOl 
+
+        public string ConsumerSecret { get; } //tepIgaeUE0wVFSuHa9z2aUvCCKTMD9tMt5ZxxyZBGWyUakhVUo 
+
+        public string ApiKey { get; } //2388862608-ZB71dptULrjtTZSHZDpZ8KEXdvcmzNRConjp3RP
+
+        public string ApiSecret { get; } //OBI8v2BQGRN3HWWKLTK1slKUxtntLjzqTz3Mj9beRx7rY 
+
+        public int PerPage { get; }
+
+        public int MaxDepth { get; }
+    }
+}
