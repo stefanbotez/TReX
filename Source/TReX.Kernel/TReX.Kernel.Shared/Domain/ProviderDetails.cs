e4a3a99b1d497e9c870f6b0423d52e2730da2232
Razvan <radeanu.razvan99@gmail.com>
2019-06-12 23:12:20 +0300

    Kernel integration changes

---
+++ Source/TReX.Kernel/TReX.Kernel.Shared/Domain/ProviderDetails.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+
+namespace TReX.Kernel.Shared.Domain
+{
+    public sealed class ProviderDetails : ValueObject
+    {
+        private ProviderDetails()
+        {
+        }
+
+        public string ExternalId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public static Result<ProviderDetails> Create(string externalId, string name)
+        {
+            var idResult = Maybe<string>.From(externalId).ToResult(KernelSharedMessages.InvalidExternalId)
+                .Ensure(i => !string.IsNullOrEmpty(i), KernelSharedMessages.InvalidExternalId);
+            var nameResult = Maybe<string>.From(name).ToResult(KernelSharedMessages.InvalidProviderName)
+                .Ensure(i => !string.IsNullOrEmpty(i), KernelSharedMessages.InvalidProviderName);
+
+            return Result.FirstFailureOrSuccess(idResult, nameResult)
+                .OnSuccess(() => new ProviderDetails
+                {
+                    ExternalId = externalId,
+                    Name = name
+                });
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return ExternalId;
+            yield return Name;
+        }
+    }
+}
